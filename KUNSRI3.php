2360a97dff9942a1adc276b4290809e8f41b3b5e
KPtch <36377768+KPtch@users.noreply.github.com>
2018-02-16 05:17:25 +0700

    Update KUNSRI3.php

--- KUNSRI3.php
+++ KUNSRI3.php
@@ -1,84 +1,33 @@
 <?php
 $access_token = "EAACdZCFrDYhoBAKYNs3Tq78I032Fxfijsd1apR7sVt1XaAWXYeLJ3OulU5L60M15hWnUT0RldRXivEp7hgGNzb47MbcOzeR2z6JGYVoN5WCVpn0e2ZCZB1epOBNSgc8dY4tZBH6CJZAz7ZCXnV5nXTrsM2GFH5QzvJczpTO9Uu1YZBADo2Lbn6HIqO6HEVByiEZD";
-$verify_token = "KUNSRI3";
-$hub_verify_token = null;
-if(isset($_REQUEST['hub_challenge'])) {
- $challenge = $_REQUEST['hub_challenge'];
- $hub_verify_token = $_REQUEST['hub_verify_token'];
-}
-if ($hub_verify_token === $verify_token) {
- echo $challenge;
-}
-$input = json_decode(file_get_contents('php://input'), true);
-$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-$message = $input['entry'][0]['messaging'][0]['message']['text'];
-$message_to_reply = '';
-/**
- * Some Basic rules to validate incoming messages
- */
 
-$api_key="b18c49146f2aa74c2482be6ac5ca71df";
-$url = 'https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'';
-$json = file_get_contents('https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$message.'"}');
-$data = json_decode($json);
-$isData=sizeof($data);
-if (strpos($message, 'hello') !== false) {
-  if (strpos($message, 'hello') !== false) {
-    $x_tra = str_replace("hello","", $message);
-    $pieces = explode("|", $x_tra);
-    $_question=str_replace("[","",$pieces[0]);
-    $_answer=str_replace("]","",$pieces[1]);
-    //Post New Data
-    $newData = json_encode(
-      array(
-        'question' => $_question,
-        'answer'=> $_answer
-      )
-    );
-    $opts = array(
-      'http' => array(
-          'method' => "POST",
-          'header' => "Content-type: application/json",
-          'content' => $newData
-       )
-    );
-    $context = stream_context_create($opts);
-    $returnValue = file_get_contents($url,false,$context);
-    $message_to_reply = 'I ready to lend';
-  }
-}else{
-  if($isData >0){
-   foreach($data as $rec){
-     $message_to_reply = $rec->answer;
-   }
-  }else{
-    $message_to_reply = 'สอนเราให้ฉลาดขึ้น : ri2[คำถาม|คำตอบ]';
-  }
-}
-//API Url
-$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
-//Initiate cURL.
-$ch = curl_init($url);
-//The JSON data.
-$jsonData = '{
+  //API Url
+  $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
+  
+
+  $input = json_decode(file_get_contents('php://input'), true);
+
+  $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+  $message = $input['entry'][0]['messaging'][0]['message']['text'];
+
+  $ch = curl_init($url);
+  $jsonData = '{
     "recipient":{
         "id":"'.$sender.'"
     },
     "message":{
-        "text":"'.$message_to_reply.'"
+        "text":"'.'test message to reply u message : '.$message.'"
     }
-}';
-//Encode the array into JSON.
-$jsonDataEncoded = $jsonData;
-//Tell cURL that we want to send a POST request.
-curl_setopt($ch, CURLOPT_POST, 1);
-//Attach our encoded JSON string to the POST fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-//Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-//Execute the request
-if(!empty($input['entry'][0]['messaging'][0]['message'])){
+  }';
+
+  $jsonDataEncoded = $jsonData;
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  if(!empty($input['entry'][0]['messaging'][0]['message'])){
     $result = curl_exec($ch);
-}
+    
+  }
+  curl_close($ch);
+
 ?>
