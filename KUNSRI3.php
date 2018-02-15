a6e0406615205d9a39ce989e3dd85176a5893e99
KPtch <36377768+KPtch@users.noreply.github.com>
2018-02-16 06:15:39 +0700

    Update KUNSRI3.php

--- KUNSRI3.php
+++ KUNSRI3.php
@@ -2,6 +2,8 @@
 
   $access_token = "EAACdZCFrDYhoBAKYNs3Tq78I032Fxfijsd1apR7sVt1XaAWXYeLJ3OulU5L60M15hWnUT0RldRXivEp7hgGNzb47MbcOzeR2z6JGYVoN5WCVpn0e2ZCZB1epOBNSgc8dY4tZBH6CJZAz7ZCXnV5nXTrsM2GFH5QzvJczpTO9Uu1YZBADo2Lbn6HIqO6HEVByiEZD";
   $verify_token = "KUNSRI3";
+
+
   $hub_verify_token = null;
   if(isset($_REQUEST['hub_challenge'])) {
     $challenge = $_REQUEST['hub_challenge'];
@@ -16,8 +18,9 @@
 
   $input    = json_decode(file_get_contents('php://input'), true);
 
-  $sender   = $input['entry'][0]['messaging'][0]['sender']['id'];
-  $message  = $input['entry'][0]['messaging'][0]['message']['text'];
+
+  $sender   = $input['entry'][0]['messaging'][0]['user_id']['accounts'];
+  $message  = $input['entry'][0]['messaging'];
 
   $ch = curl_init($url);
   $jsonData = '{
@@ -25,7 +28,7 @@
           "id":"'.$sender.'"
       },
       "message":{
-          "text":"'.'test message to reply u message : '.$message.'"
+          "text":"'.'test message to reply u message : '.$response.'"
       }
     }';
 
