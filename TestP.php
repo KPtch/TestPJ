78c2dd67426113cdb48dd925e4db7150c4fea94d
KPtch <36377768+KPtch@users.noreply.github.com>
2018-04-01 00:49:47 +0700

    Create TestP.php

---
+++ TestP.php
@@ -0,0 +1,188 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Marcuscode.com</title>
+</head>
+<body>
+<p align="center"><font color="#006600" size="2" face="MS Sans Serif, Tahoma, sans-serif"><strong>
+</strong><strong>เอกสารคำร้อง <br />
+</strong>( ใช้โปรแกรม Adobe Acrobat ในการดาวน์โหลด)</font></p>
+<table width="99%" border="0" cellspacing="1">
+ 
+ <tr>
+  <td width="2%" valign="top"><div align="center"> </div></td>
+  <td width="98%" bgcolor="#CCCCCC"><div align="left"> <span class="style23">
+   
+   </span>
+      <table width="616" border="0" cellpadding="1" cellspacing="1">
+       
+       <tr>
+        <td width="113" class="style24">Registrar-1</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/1 คำร้องทั่วไป.pdf"  target="_blank"><b>คำร้องทั่วไป</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-2</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/ขอเอกสารทางการศึกษา-new (1).pdf"  target="_blank"><b>คำร้องขอเอกสารทางการศึกษา</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-3</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/3 ขอผ่อนผัน(แก้ไข).pdf"  target="_blank"><b>คำร้องขอผ่อนผัน</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-4</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/4 คำร้องของดเรียนเหลือต่ำกว่า 9 หน่วย.pdf"  target="_blank"><b>คำร้องของดเรียนเหลือต่ำกว่า 9 หน่วย</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-5</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/5 เรียนต่ำกว่า 9 หน่วย(ปีสุดท้าย).pdf"  target="_blank"><b>เรียนต่ำกว่า 9 หน่วย(ปีสุดท้าย)</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-6</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/6 คำร้องขอเรียนเกิน - ต่ำ.pdf"  target="_blank"><b>คำร้องขอเรียนเกิน - ต่ำ</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-7</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/7 คำร้องขอ Regrade.pdf"  target="_blank"><b>คำร้องขอ Regrade</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-8</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/8 คำร้องขอลงทะเบียนเรียน Audit.pdf"  target="_blank"><b>คำร้องขอลงทะเบียนเรียน Audit</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-9</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/9 คำร้องขอเรียนข้าม วิทยาเขต.pdf"  target="_blank"><b>คำร้องขอเรียนข้าม วิทยาเขต</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-10</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/10 ขอลงทะเบียนเรียนล่าช้า.pdf"  target="_blank"><b>ขอลงทะเบียนเรียนล่าช้า</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-11</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/11 ขอคืนเงิน(แก้ไข).pdf"  target="_blank"><b>ขอคืนเงิน</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-12</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/12 ขอย้ายคณะ.pdf"  target="_blank"><b>ขอย้ายคณะ</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-13</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/13 ขอย้ายสาขา.pdf"  target="_blank"><b>ขอย้ายสาขา</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-14</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/14 ขอเทียบรายวิชา.pdf"  target="_blank"><b>ขอเทียบรายวิชา</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-15</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/15 ขอเทียบโอนรายวิชา.pdf"  target="_blank"><b>ขอเทียบโอนรายวิชา</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-16</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/16 ขอเรียนควบ.pdf"  target="_blank"><b>ขอเรียนควบ</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-17</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/17 ขอสอบชดใช้(กรณีป่วย).pdf"  target="_blank"><b>ขอสอบชดใช้(กรณีป่วย)</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-18</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/18 ขอสอบชดใช้.pdf"  target="_blank"><b>ขอสอบชดใช้</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-19</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/19 ขอลาพักการศึกษา.pdf"  target="_blank"><b>ขอลาพักการศึกษา</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-20</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/20 ขอรักษาสถานภาพนิสิต.pdf"  target="_blank"><b>ขอรักษาสถานภาพนิสิต</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-21</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/21 ขอคืนสภาพนิสิต.pdf"  target="_blank"><b>ขอคืนสภาพนิสิต</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-22</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/22 ขอกลับเข้าศึกษาต่อ.pdf"  target="_blank"><b>ขอกลับเข้าศึกษาต่อ</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-23</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/23 ขอลาออก.pdf"  target="_blank"><b>ขอลาออก</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-24</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/24 ประกันของเสียหาย (แก้ไข).pdf"  target="_blank"><b>ประกันของเสียหาย</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-25</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/25 ใบลา.pdf"  target="_blank"><b>ใบลา</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-26</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/26 ขอรหัสลงทะเบียนเรียน.doc"  target="_blank"><b>ขอรหัสลงทะเบียนเรียน</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-28</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/28 ขอเปลี่ยนแปลงชื่อ-สกุล.pdf"  target="_blank"><b>ขอเปลี่ยนแปลงชื่อ-สกุล</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-29</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/29 ขอเปลี่ยนแปลงทีอยู่ผู้ปกครอง.pdf"  target="_blank"><b>ขอเปลี่ยนแปลงทีอยู่ผู้ปกครอง</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-30</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/30 คำร้องขอลงทะเบียนเรียนกรณีเกรดต่ำกว่า 2.00.pdf"  target="_blank"><b>คำร้องขอลงทะเบียนเรียนกรณีเกรดต่ำกว่า 2.00</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-31</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/31 ขอลงทะเบียนเรียน-ปรับปรุง.pdf"  target="_blank"><b>ขอลงทะเบียนเรียน-ปรับปรุง</b></a></td>
+       </tr>
+       
+       <tr>
+        <td width="113" class="style24">Registrar-32</td>
+        <td width="489"><a  href="https://reg.src.ku.ac.th/student/news/kamrong/nisit.pdf"  target="_blank"><b>แนวปฏิบัติสำหรับนิสิตในการเข้าสอบ มหาวิทยาลัยเกษตรศาสตร์</b></a></td>
+       </tr>
+       
+      </table>
+  </div></td>
+ </tr>
+</table>
+
+</body>
+</html>
+
+
+
+
+
+
